a3a7ae904f2b41b43b1dea196c1fb76c9b8ddf30
Alexander Lopez <alexander.lopez@owasp.org>
2023-05-25 19:50:48 -0400

    Remove Host Header Middleware

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from routers import characters, episodes, locations, characters_appearances_by_episodes, locations_visited_by_characters
 from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
-from fastapi.middleware.trustedhost import TrustedHostMiddleware
 
 description = """
 Query information about HBO Max's Velma
@@ -18,9 +17,6 @@ app = FastAPI(
 )
 
 app.add_middleware(HTTPSRedirectMiddleware)
-app.add_middleware(
-    TrustedHostMiddleware, allowed_hosts=["thevelmashow.com", "api.thevelmashow.com"]
-)
 
 app.include_router(characters.router)
 app.include_router(episodes.router)
