fd2177a9f15d4220c1f1d2ab1661f8dcc40aa980
Alexander Lopez <alexander.lopez@owasp.org>
2023-05-26 07:50:28 -0400

    Remove the DATABASE_URL_2 variable

--- app/config/variables.py
+++ app/config/variables.py
@@ -3,7 +3,6 @@ import os
 
 class Settings(BaseSettings):
     DATABASE_URL=os.getenv("DATABASE_URL")
-    DATABASE_URL_2=os.getenv("DATABASE_URL_2")
     DOMAIN = os.getenv("DOMAIN")
     API_AUDIENCE = os.getenv("API_AUDIENCE")
     ISSUER = os.getenv("ISSUER")
