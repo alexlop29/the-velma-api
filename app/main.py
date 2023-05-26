1fc0185c5aa00594bd85c449640210053d8b0f55
Alexander Lopez <alexander.lopez@owasp.org>
2023-05-25 20:06:41 -0400

    Redirect to Docs in Root

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from routers import characters, episodes, locations, characters_appearances_by_episodes, locations_visited_by_characters
 from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
+from fastapi.responses import RedirectResponse
 
 description = """
 Query information about HBO Max's Velma
@@ -27,3 +28,6 @@ app.include_router(locations_visited_by_characters.router)
 @app.get("/", include_in_schema=False)
 async def root():
     return {"message": "the-velma-api"}
+
+async def root():
+    return RedirectResponse(url='/docs')
