6650eedd5939517371f48f2a7d6f9f9a5b61d732
Alexander Lopez <alexander.lopez@owasp.org>
2023-05-27 10:52:35 -0400

    Try returning character_to_delete to determine 404 response

--- app/routers/characters.py
+++ app/routers/characters.py
@@ -134,6 +134,7 @@ async def delete_character(
     result = VerifyToken(token.credentials).verify()
     try:
         character_to_delete = db.query(Character).filter(Character.character_id==id)
+        return character_to_delete
         if character_to_delete is None:
             sentry_sdk.capture_message(character_to_delete)
             return HTTPException(status_code=404, detail="Not found")
