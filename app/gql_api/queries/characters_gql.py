cf9ad78b3d0a2a26cf84a3513f7b9c3266358ed2
Alexander Lopez <alexander.lopez@owasp.org>
2023-06-03 19:08:07 -0400

    Add pagination to graphql/characters

--- app/gql_api/queries/characters_gql.py
+++ app/gql_api/queries/characters_gql.py
@@ -9,9 +9,52 @@ from fastapi import Depends
 from typing import Optional
 from enum import Enum
 import typing
+from typing import List, TypeVar
 from strawberry.scalars import JSON
 
 db = SessionLocal()
+
+GenericType = TypeVar("GenericType")
+
+@strawberry.type
+class PaginationWindow(List[GenericType]):
+    items: List[GenericType] = strawberry.field(
+        description="The list of items in this pagination window."
+    )
+
+    total_items_count: int = strawberry.field(
+        description="Total number of items in the filtered dataset."
+    )
+
+def get_pagination_window(
+        dataset: List[GenericType],
+        ItemType: type,
+        limit: int,
+        offset: int = 0,
+        filters: dict[str, str] = {}) -> PaginationWindow:
+    """
+    Get one pagination window on the given dataset for the given limit
+    and offset, ordered by the given attribute and filtered using the
+    given filters
+    """
+
+    if limit <= 0 or limit > 100:
+        raise Exception(f'limit ({limit}) must be between 0-100')
+
+    if offset != 0 and not 0 <= offset < len(dataset):
+        raise Exception(f'offset ({offset}) is out of range '
+                        f'(0-{len(dataset) - 1})')
+
+    total_items_count = len(dataset)
+
+    items = dataset[offset:offset + limit]
+
+    items = [ItemType.from_row(x) for x in items]
+
+    return PaginationWindow(
+        items=items,
+        total_items_count=total_items_count
+    )
 
 @strawberry.enum
 class SelectCharacterSearchField(Enum):
@@ -26,8 +69,11 @@ class SearchCharacterPath:
 @strawberry.type
 class Character_GQL:
     @strawberry.field
-    def get_characters(self) -> list[Schema_Char]:
-        return db.query(Character).order_by(Character.first_name)
+    def get_characters(self, limit: int, offset: int = 0,) -> PaginationWindow[Schema_Char]:
+        return get_pagination_window(dataset=db.query(Character).order_by(Character.first_name), \
+                                    ItemType=Character, \
+                                    limit=limit, \
+                                    offset=offset,)
     @strawberry.field
     def search_for_character(self, search_options: SearchCharacterPath) -> list[Schema_Char]:
         print(search_options.search_field, search_options.search_string)
