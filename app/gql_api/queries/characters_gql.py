181f2c7a43222397c5031ff5b6f6c763278b8e37
Alexander Lopez <alexander.lopez@owasp.org>
2023-06-05 14:51:39 -0400

    Remediate flake8 warnings in app/gql_api/queries

--- app/gql_api/queries/characters_gql.py
+++ app/gql_api/queries/characters_gql.py
@@ -2,48 +2,49 @@ import strawberry
 from gql_api.schemas.characters import Character as Schema_Char
 from models.characters import Character
 from config.db import SessionLocal
-import strawberry
-from sqlalchemy.orm import Session
-from sqlalchemy import or_
-from fastapi import Depends
-from typing import Optional
 from enum import Enum
-import typing
-from typing import List, TypeVar
 from strawberry.scalars import JSON
 from gql_api.fragments.pagination import PaginationWindow, get_pagination_window
 
 db = SessionLocal()
+
 
 @strawberry.enum
 class SelectCharacterSearchField(Enum):
     first_name = "first_name"
     last_name = "last_name"
 
+
 @strawberry.input
 class SearchCharacterPath:
     search_field: SelectCharacterSearchField
-    search_string : str
+    search_string: str
+
 
 @strawberry.type
 class Character_GQL:
     @strawberry.field
     def get_characters(self, limit: int, offset: int = 0,) -> PaginationWindow[Schema_Char]:
         count = db.query(Character).count()
-        return get_pagination_window(dataset=db.query(Character).order_by(Character.first_name), \
-                                    ItemType=Character, \
-                                    limit=limit, \
-                                    offset=offset,
-                                    total=count)
+        return get_pagination_window(dataset=db.query(Character).order_by(Character.first_name),
+                                     ItemType=Character,
+                                     limit=limit,
+                                     offset=offset,
+                                     total=count)
+
     @strawberry.field
     def search_for_character(self, search_options: SearchCharacterPath) -> list[Schema_Char]:
         print(search_options.search_field, search_options.search_string)
         match search_options.search_field:
             case SelectCharacterSearchField.first_name:
-                return db.query(Character).filter(Character.first_name.ilike(f'%{search_options.search_string}%')).all()
+                return db.query(Character).filter(
+                    Character.first_name.ilike(f'%{search_options.search_string}%')
+                ).all()
             case SelectCharacterSearchField.last_name:
-                return db.query(Character).filter(Character.last_name.ilike(f'%{search_options.search_string}%')).all()
+                return db.query(Character).filter(
+                    Character.last_name.ilike(f'%{search_options.search_string}%')
+                ).all()
+
     @strawberry.field
     def count_of_characters(self) -> JSON:
-        return {"count": db.query(Character).count()} 
-
+        return {"count": db.query(Character).count()}
--- app/gql_api/queries/locations_gql.py
+++ app/gql_api/queries/locations_gql.py
@@ -2,29 +2,24 @@ import strawberry
 from gql_api.schemas.locations import Location as Location_Schema
 from models.locations import Location
 from config.db import SessionLocal
-import strawberry
-from sqlalchemy.orm import Session
-from sqlalchemy import or_
-from fastapi import Depends
-from typing import Optional
-from enum import Enum
-import typing
 from strawberry.scalars import JSON
 from gql_api.fragments.pagination import PaginationWindow, get_pagination_window
 
+
 db = SessionLocal()
+
 
 @strawberry.type
 class Location_GQL:
     @strawberry.field
     def get_locations(self, limit: int, offset: int = 0,) -> PaginationWindow[Location_Schema]:
         count = db.query(Location).count()
-        return get_pagination_window(dataset=db.query(Location).order_by(Location.name), \
-                                    ItemType=Location, \
-                                    limit=limit, \
-                                    offset=offset,
-                                    total=count)
+        return get_pagination_window(dataset=db.query(Location).order_by(Location.name),
+                                     ItemType=Location,
+                                     limit=limit,
+                                     offset=offset,
+                                     total=count)
+
     @strawberry.field
     def count_of_locations(self) -> JSON:
-        return {"count": db.query(Location).count()} 
-
+        return {"count": db.query(Location).count()}
